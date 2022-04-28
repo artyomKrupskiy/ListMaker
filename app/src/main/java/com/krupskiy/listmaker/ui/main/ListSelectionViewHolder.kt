4921326f20327dddd57ad8bae9cd48fdd08262bb
Artyom Krupskiy <kru5a777@gmail.com>
2022-04-29 00:00:40 +0300

    feat: RecyclerView added and mocked data

---
+++ app/src/main/java/com/krupskiy/listmaker/ui/main/ListSelectionViewHolder.kt
@@ -0,0 +1,8 @@
+package com.krupskiy.listmaker.ui.main
+
+import androidx.recyclerview.widget.RecyclerView
+import com.krupskiy.listmaker.databinding.ListSelectionViewHolderBinding
+
+class ListSelectionViewHolder(val binding: ListSelectionViewHolderBinding) : RecyclerView.ViewHolder(binding.root) {
+
+}
